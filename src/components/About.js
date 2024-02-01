85dca326cf5891fc2f140e29ab7f3c58fffe7336
ChepkemoiS <memoi24sharon@gmail.com>
2024-02-02 04:35:38 +0630

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Chepkemoi</p>
+      <img src={image} alt="I did this" />
+    </div>
+  ) 
+  
 }
 
 export default About;
